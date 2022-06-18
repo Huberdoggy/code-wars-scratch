74864475c541f5396ae474c257ca9e45f7ad5219
Huberdoggy <huberdoggy@gmail.com>
2022-06-18 13:44:46 -0500

    completed some more CPP challenges

---
+++ cpp/8kyu/can-we-divide.cpp
@@ -0,0 +1,45 @@
+// Your task is to create the functionisDivideBy(or is_divide_by) to check
+//     if an integer number is divisible by both integers a and b
+// 	.
+
+//     A few cases :
+
+//     (-12, 2, -6)
+// 	->true(-12, 2, -5)
+// 	->false
+
+//     (45, 1, 6)
+// 	->false(45, 5, 15)
+// 	->true
+
+//     (4, 1, 4)
+// 	->true(15, -5, 3)
+// 	->true
+
+#include <iostream>
+
+using namespace std;
+
+bool isDivideBy(int number, int a, int b)
+{
+	if (number % a == 0 && number % b == 0) {
+		return true;
+	}
+	return false;
+}
+
+int main()
+{
+	int num1{}, num2{}, num3{};
+
+	cout << "Enter a number: ";
+	cin >> num1;
+	cout << "\nAnd another number: ";
+	cin >> num2;
+	cout << "\nNow one more: ";
+	cin >> num3;
+
+	bool result = isDivideBy(num1, num2, num3);
+	cout << "\n\n" << result;
+	return 0;
+}
---
+++ cpp/8kyu/what-century-is-it.cpp
@@ -0,0 +1,36 @@
+// The first century spans from the year 1 up to and including the year 100,
+//     the second century - from the year 101 up to and including the year 200,
+//     etc.
+
+//     Task Given a year,
+//     return the century it is in.
+
+// 	   Examples 1705 -- > 18 1900 -- > 19 1601 -- > 17 2000 -- > 20
+
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int centuryFromYear(int year)
+{
+	int resultYear{};
+	double dblCentury = (double)year;
+	double decNum = dblCentury / 100;
+	int truncate = (int)decNum;
+	double diff = decNum - truncate;
+	diff >= 0.01 ? resultYear = truncate + 1 : resultYear = truncate;
+	return resultYear;
+}
+
+int main()
+{
+	vector<int> centuries{1601, 1705, 1900, 2000};
+
+	for (int x : centuries)
+
+		printf("\nYear %d is in the %dth century.", x,
+		       centuryFromYear(x));
+	return 0;
+}
